237d59dfa3d69044e4f4139e256b516e5dafc79d
fa-kohansal <farnaz_kohansal@yahoo.com>
2024-12-24 04:33:26 -0800

    set child.tsx

--- src/Components/child.tsx
+++ src/Components/child.tsx
@@ -1,6 +1,8 @@
 import { useStore } from "../store";
+
 export function Child() {
-  const { count } = useStore((state) => state);
+  const { count } = useStore((state: { count: number }) => state);
+
   return (
     <div>
       <p>the count is: {count}</p>
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
-type Store={
-    count:number;
-    increaseCount: () => void;
-    decreaseCount: () => void;
+
+type Store = {
+  count: number;
+  increaseCount: () => void;
+  decreaseCount: () => void;
 };
+
 export const useStore = create<Store>((set) => ({
   count: 0,
   increaseCount: () => set((state) => ({ count: state.count + 1 })),
