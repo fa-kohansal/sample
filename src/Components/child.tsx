6808d93caea27a5e24683a47d42d10311422dd45
fa-kohansal <farnaz_kohansal@yahoo.com>
2024-12-24 04:36:33 -0800

    set child.tsx

--- src/Components/child.tsx
+++ src/Components/child.tsx
@@ -3,6 +3,7 @@ import { useStore } from "../store";
 export function Child() {
   const { count } = useStore((state: { count: number }) => state);
 
+  
   return (
     <div>
       <p>the count is: {count}</p>
